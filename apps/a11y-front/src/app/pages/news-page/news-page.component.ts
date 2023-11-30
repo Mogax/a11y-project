56a108b355d4a0bb4c4604bff27f3046e927c8c1
Jarquen <quentin.jarlot@gmail.com>
2023-11-30 21:06:20 +0100

    Add French translation

--- apps/a11y-front/src/app/pages/news-page/news-page.component.ts
+++ apps/a11y-front/src/app/pages/news-page/news-page.component.ts
@@ -1,33 +1,30 @@
-import {Component, inject} from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {HeaderComponent} from "../../components/header/header.component";
-import {FooterComponent} from "../../components/footer/footer.component";
-import {NewsService} from "./news.service";
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { NewsService } from './news.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'a11y-news-page',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, FooterComponent],
+  imports: [CommonModule, HeaderComponent, FooterComponent, TranslateModule],
   templateUrl: './news-page.component.html',
   styleUrls: ['./news-page.component.scss'],
 })
-
 export class NewsPageComponent {
-
   constructor() {
-    this.setNotifications()
+    this.setNotifications();
   }
 
   private readonly newsService = inject(NewsService);
-  allNews$ = this.newsService.getNews() ;
+  allNews$ = this.newsService.getNews();
 
   private setNotifications() {
     Notification.requestPermission().then((permission) => {
-      if (permission === "granted") {
+      if (permission === 'granted') {
         // To use later
       }
     });
   }
-
 }
-
--- apps/a11y-front/src/app/pages/not-allowed-logged-out-page/not-allowed-logged-out-page.component.ts
+++ apps/a11y-front/src/app/pages/not-allowed-logged-out-page/not-allowed-logged-out-page.component.ts
@@ -1,13 +1,13 @@
-import {Component} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterLink} from "@angular/router";
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'a11y-not-allowed-logged-out-page',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, TranslateModule],
   templateUrl: './not-allowed-logged-out-page.component.html',
   styleUrls: ['./not-allowed-logged-out-page.component.scss'],
 })
-export class NotAllowedLoggedOutPageComponent {
-}
+export class NotAllowedLoggedOutPageComponent {}
