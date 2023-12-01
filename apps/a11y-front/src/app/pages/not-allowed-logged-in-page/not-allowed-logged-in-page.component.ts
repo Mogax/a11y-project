9084367fc4b37e0353f7abcbd05effda9b5fa80c
mogax <mogasrx@gmail.com>
2023-12-01 16:16:28 +0100

    Add header all pages

--- apps/a11y-front/src/app/pages/not-allowed-logged-in-page/not-allowed-logged-in-page.component.ts
+++ apps/a11y-front/src/app/pages/not-allowed-logged-in-page/not-allowed-logged-in-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import {HeaderComponent} from "../../components/header/header.component";
 
 @Component({
   selector: 'a11y-not-allowed-logged-in-page',
   standalone: true,
-  imports: [CommonModule, RouterLink, TranslateModule],
+    imports: [CommonModule, RouterLink, TranslateModule, HeaderComponent],
   templateUrl: './not-allowed-logged-in-page.component.html',
   styleUrls: ['./not-allowed-logged-in-page.component.scss'],
 })
