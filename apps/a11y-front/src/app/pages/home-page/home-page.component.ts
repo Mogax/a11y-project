88799880a491d06b1f46ac336cd47bbf2efbf4ac
mogax <mogasrx@gmail.com>
2023-12-08 14:24:40 +0100

    fix merge conflict

--- apps/a11y-front/src/app/pages/home-page/home-page.component.ts
+++ apps/a11y-front/src/app/pages/home-page/home-page.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
-import {FooterComponent} from "../../components/footer/footer.component";
+import { FooterComponent } from "../../components/footer/footer.component";
+import {HeaderComponent} from "../../components/header/header.component";
 
 @Component({
   selector: 'a11y-home-page',
